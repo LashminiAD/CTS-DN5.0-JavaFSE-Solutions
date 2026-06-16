c2f93fe7c491edeac0865efb9e925ceb90371098
LashminiAD <lashminianand25@gmail.com>
2026-06-16 18:00:34 +0530

    Week1 - Ex02 Factory Method Pattern completed

---
+++ Week 1/Design_Patterns/Ex02_FactoryMethod_Pattern/Document.java
@@ -0,0 +1,6 @@
+// Author: Lashmini A D
+// CTS DN 5.0 - Week1 - Exercise02 - Factory Method Pattern
+
+public interface Document {
+    void open();
+}
---
+++ Week 1/Design_Patterns/Ex02_FactoryMethod_Pattern/Main.java
@@ -0,0 +1,19 @@
+// Author: Lashmini A D
+// CTS DN 5.0 - Week1 - Exercise02 - Factory Method Pattern
+
+public class Main {
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory = new WordFactory();
+        Document word = wordFactory.createDocument();
+        word.open();
+
+        DocumentFactory pdfFactory = new PdfFactory();
+        Document pdf = pdfFactory.createDocument();
+        pdf.open();
+
+        DocumentFactory excelFactory = new ExcelFactory();
+        Document excel = excelFactory.createDocument();
+        excel.open();
+    }
+}
