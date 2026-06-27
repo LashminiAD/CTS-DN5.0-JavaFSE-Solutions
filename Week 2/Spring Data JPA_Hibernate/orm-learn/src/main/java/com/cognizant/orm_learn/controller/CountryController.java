c8f1170bbbd625e4bb906de1b315bbfe66dabd30
LashminiAD <lashminianand25@gmail.com>
2026-06-27 23:28:58 +0530

    Spring Data JPA Quick Example - Country entity, repository, service, controller - COMPLETED

---
+++ Week 2/Spring Data JPA_Hibernate/orm-learn/src/main/java/com/cognizant/orm_learn/controller/CountryController.java
@@ -0,0 +1,24 @@
+package com.cognizant.orm_learn.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
---
+++ Week 2/Spring Data JPA_Hibernate/orm-learn/src/main/java/com/cognizant/orm_learn/model/Country.java
@@ -0,0 +1,49 @@
+package com.cognizant.orm_learn.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    // No-arg constructor - required by JPA/Hibernate to create instances via reflection
+    public Country() {
+    }
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country [code=" + code + ", name=" + name + "]";
+    }
+}
