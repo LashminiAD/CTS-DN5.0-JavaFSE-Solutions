f246b3129cf2e79d8a4473a401ece731559f006c
LashminiAD <lashminianand25@gmail.com>
2026-06-28 14:43:34 +0530

    Week 2: Completed Exercise 4 - Spring Data JPA Query Method

--- Week 2/Spring Data JPA_Hibernate/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
+++ Week 2/Spring Data JPA_Hibernate/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -1,13 +1,65 @@
 package com.cognizant.orm_learn;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+
 @SpringBootApplication
-public class OrmLearnApplication {
+public class OrmLearnApplication implements CommandLineRunner {
+	@Autowired
+	private CountryRepository countryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrmLearnApplication.class, args);
 	}
+	@Override
+    public void run(String... args) throws Exception {
+
+        // Uncomment ONLY ONE method at a time
+
+        // testSearchContaining();
+
+        // testSearchContainingSorted();
+
+         testStartsWith();	
+	}
+
+	// Query 1 - Search countries containing "ou"
+	public void testSearchContaining() {
+	System.out.println("\n===== Countries containing 'ou' =====");
+
+        List<Country> countries = countryRepository.findByNameContaining("ou");
+
+        countries.forEach(System.out::println);
+}
+
+    // Query 2 - Search countries containing "ou" in ascending order
+
+
+public void testSearchContainingSorted() {
+        System.out.println("\n===== Countries containing 'ou' (Sorted) =====");
+
+    List<Country> countries =
+        countryRepository.findByNameContainingOrderByNameAsc("ou");
+
+    countries.forEach(System.out::println);
+
+}
+    // Query 3 - Search countries starting with "Z"
+
+public void testStartsWith() {
+		System.out.println("\n===== Countries starting with 'Z' =====");
+    List<Country> countries =
+        countryRepository.findByNameStartingWith("Z");
+
+    countries.forEach(System.out::println);
+
+}
 
 }
--- Week 2/Spring Data JPA_Hibernate/orm-learn/src/main/java/com/cognizant/orm_learn/repository/CountryRepository.java
+++ Week 2/Spring Data JPA_Hibernate/orm-learn/src/main/java/com/cognizant/orm_learn/repository/CountryRepository.java
@@ -1,5 +1,7 @@
 package com.cognizant.orm_learn.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,9 @@ import com.cognizant.orm_learn.model.Country;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, String> {
+    List<Country> findByNameContaining(String text);
+
+    List<Country> findByNameContainingOrderByNameAsc(String text);
+
+    List<Country> findByNameStartingWith(String alphabet);
 }
