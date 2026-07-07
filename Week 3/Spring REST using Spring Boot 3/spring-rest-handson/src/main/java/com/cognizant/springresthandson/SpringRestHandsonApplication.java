e186d4beb6ffb893c991671ed5b56dcf48d57090
LashminiAD <lashminianand25@gmail.com>
2026-07-08 05:27:26 +0530

    Week 3 Ex2: Load Country bean from Spring XML configuration

--- Week 3/Spring REST using Spring Boot 3/spring-rest-handson/src/main/java/com/cognizant/springresthandson/SpringRestHandsonApplication.java
+++ Week 3/Spring REST using Spring Boot 3/spring-rest-handson/src/main/java/com/cognizant/springresthandson/SpringRestHandsonApplication.java
@@ -3,11 +3,23 @@ package com.cognizant.springresthandson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cognizant.springresthandson.model.Country;
+
 @SpringBootApplication
 public class SpringRestHandsonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestHandsonApplication.class, args);
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+		Country country = context.getBean("country", Country.class);
+
+		System.out.println(country);
 	}
 
 }
+
