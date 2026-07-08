4ce9a4844e94944e13803a6b24ae631589f1b208
LashminiAD <lashminianand25@gmail.com>
2026-07-08 06:09:57 +0530

    Week 3 Ex4: Created Country REST API returning JSON response

--- Week 3/Spring REST using Spring Boot 3/spring-rest-handson/src/main/java/com/cognizant/springresthandson/SpringRestHandsonApplication.java
+++ Week 3/Spring REST using Spring Boot 3/spring-rest-handson/src/main/java/com/cognizant/springresthandson/SpringRestHandsonApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cognizant.springresthandson.model.Country;
 
 @SpringBootApplication
+@ImportResource("classpath:country.xml")
 public class SpringRestHandsonApplication {
 
 	public static void main(String[] args) {
---
+++ Week 3/Spring REST using Spring Boot 3/spring-rest-handson/src/main/java/com/cognizant/springresthandson/controller/CountryController.java
@@ -0,0 +1,19 @@
+package com.cognizant.springresthandson.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springresthandson.model.Country;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private Country country;
+
+    @GetMapping("/country")
+    public Country getCountry() {
+        return country;
+    }
+}
