ac51607e677c0f17c8bbb274fc4a81ca72dcc047
LashminiAD <lashminianand25@gmail.com>
2026-07-17 18:56:22 +0530

    Added HOL1 - CRA setup with output screenshot

---
+++ Week 5/react_hol1/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Lashmini's React Journey</h1>
+      <p>Week 5, Day 1 - HOL 1: CRA Setup</p>
+    </div>
+  );
+}
+
+export default App;
