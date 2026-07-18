a36b05c75769741ef7acc6262489ae6fe851841b
LashminiAD <lashminianand25@gmail.com>
2026-07-18 14:00:35 +0530

    Added HOL 5 - React Styling using CSS Modules

---
+++ Week 5/react_hol5/src/App.js
@@ -0,0 +1,12 @@
+import CohortDetails from "./components/CohortDetails";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <CohortDetails />
+    </div>
+  );
+}
+
+export default App;
